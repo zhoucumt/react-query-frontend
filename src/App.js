80e76e9f4d609ecec91faabf08323e02d7be4354
zhoucumt <553817478@qq.com>
2022-05-13 22:04:49 +0800

    当浏览器tab从其他页面切换回来时自动发送请求

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import request from './request';
 
 function Users() {
-  const { data, isLoading, isError } = useQuery('users', () =>
-    request.get('/users')
+  const { data, isLoading, isError, isFetching } = useQuery(
+    'users',
+    () => request.get('/users'),
+    {
+      refetchOnWindowFocus: true,
+    }
   );
   if (isLoading) return <div>加载中.......</div>;
   if (isError) return <div>加载失败</div>;
   return (
-    <ul>
-      {data?.map(user => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {data?.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      {isFetching && <div>正在更新数据...</div>}
+    </>
   );
 }
 function App() {
